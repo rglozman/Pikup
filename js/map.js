a2b3552818f8ef4319a232be22bf5b770413eba6
Ben Krawitz <benkraw@umich.edu>
2014-09-20 02:07:41 -0400

    marker message

--- js/map.js
+++ js/map.js
@@ -1,50 +1,38 @@
-var map;
-var markers = [];
-
-function initialize() {
-  var haightAshbury = [
-  new google.maps.LatLng(43.4630965,-80.52217417),
-  new google.maps.LatLng(43.4770042,-80.544263216),
-  new google.maps.LatLng(43.469249,-80.527782416),
-  new google.maps.LatLng(43.4610515,-80.5561899),
-  ];
-
-  var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(43.4630965,-80.52217417),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  addMarker(haightAshbury);
-}
-
-function addMarker(location) {
-  for (var i = 0; i <location.length; i++){
-    var marker = new google.maps.Marker({
-      position: location[i],
-      map: map
-    });
-    markers.push(marker);
-  }
-}
-
-// Sets the map on all markers in the array.
-function setAllMap(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setAllMap(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setAllMap(map);
+var LocationData = [
+    [43.4630965,-80.52217417, "95 King Street South, Waterloo" ], 
+    [43.4770042,-80.544263216, "151 Columbia Street, Waterloo" ], 
+    [43.469249,-80.527782416, "109 King Street North, Waterloo" ], 
+    [43.4610515,-80.5561899, "200 University Avenue Lex, Waterloo" ] 
+];
+ 
+function initialize()
+{
+    var map = 
+        new google.maps.Map(document.getElementById('map-canvas'));
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow();
+     
+    for (var i in LocationData)
+    {
+        var p = LocationData[i];
+        var latlng = new google.maps.LatLng(p[0], p[1]);
+        bounds.extend(latlng);
+         
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: p[2]
+        });
+     
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(this.title);
+            infowindow.open(map, this);
+        });
+    }
+     
+    map.fitBounds(bounds);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
