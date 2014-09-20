60735907cdb31ee6d59f402a95842524a3756a2d
rglozman <rglozman@uwaterloo.ca>
2014-09-20 03:30:55 -0400

    Updated Not For Profit View and CSS

--- js/map.js
+++ js/map.js
@@ -1,9 +1,56 @@
-      function initialize() {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+var LocationData = [
+    [43.4630965,-80.52217417, "95 King Street South, Waterloo", "1" ], 
+    [43.4770042,-80.544263216, "151 Columbia Street, Waterloo", "2" ], 
+    [43.469249,-80.527782416, "109 King Street North, Waterloo", "3" ], 
+    [43.4610515,-80.5561899, "200 University Avenue Lex, Waterloo", "4" ] 
+];
+
+
+
+function initialize()
+{
+
+    var mapOptions = {
+    disableDefaultUI: true,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = 
+        new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow();
+
+     
+    for (var i in LocationData)
+    {
+        var p = LocationData[i];
+        var latlng = new google.maps.LatLng(p[0], p[1]);
+        bounds.extend(latlng);
+         
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: p[3]
+        });
+     
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(this.title);
+            infowindow.open(map, this);
+        });
+    }
+     
+    map.fitBounds(bounds);
+
+
+}
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
