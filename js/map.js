35533aefe2a0721a1f93d5fc1690fa76ec161374
Ben Krawitz <benkraw@umich.edu>
2014-09-20 01:41:13 -0400

    js markers

--- js/map.js
+++ js/map.js
@@ -1,9 +1,42 @@
-      function initialize() {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+var map;
+var markers = [];
+
+function initialize() {
+  var haightAshbury = new google.maps.LatLng(37.7699298, -122.4469157);
+  var mapOptions = {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  addMarker(haightAshbury);
+}
+
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
