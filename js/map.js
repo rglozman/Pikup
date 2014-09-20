e83186fc3c94c50ae3987ac8069fb7ea39394058
Ben Krawitz <benkraw@umich.edu>
2014-09-20 02:26:26 -0400

    updates

--- js/map.js
+++ js/map.js
@@ -1,16 +1,28 @@
 var LocationData = [
-    [43.4630965,-80.52217417, "95 King Street South, Waterloo" ], 
-    [43.4770042,-80.544263216, "151 Columbia Street, Waterloo" ], 
-    [43.469249,-80.527782416, "109 King Street North, Waterloo" ], 
-    [43.4610515,-80.5561899, "200 University Avenue Lex, Waterloo" ] 
+    [43.4630965,-80.52217417, "95 King Street South, Waterloo", "1" ], 
+    [43.4770042,-80.544263216, "151 Columbia Street, Waterloo", "2" ], 
+    [43.469249,-80.527782416, "109 King Street North, Waterloo", "3" ], 
+    [43.4610515,-80.5561899, "200 University Avenue Lex, Waterloo", "4" ] 
 ];
  
 function initialize()
 {
+
+    var mapOptions = {
+    disableDefaultUI: true,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
     var map = 
-        new google.maps.Map(document.getElementById('map-canvas'));
+        new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var bounds = new google.maps.LatLngBounds();
     var infowindow = new google.maps.InfoWindow();
+
      
     for (var i in LocationData)
     {
@@ -21,7 +33,7 @@ function initialize()
         var marker = new google.maps.Marker({
             position: latlng,
             map: map,
-            title: p[2]
+            title: p[3]
         });
      
         google.maps.event.addListener(marker, 'click', function() {
@@ -31,7 +43,11 @@ function initialize()
     }
      
     map.fitBounds(bounds);
+
+
 }
+
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
